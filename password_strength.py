cc75acc41b3c28ad42200711a0e1d959aaa273e3
Pavel Piskunov <pspiskunov@gmail.com>
2018-03-05 22:00:25 +0300

    скрыт вводимый пароль, упрощён код, названия переменных однозначно говорят о выполняемой функции, добавлен отдельный файл под список ``плохих паролей``, удалены глобальные переменные, под магические числа созданы переменные, описывающие их суть

--- password_strength.py
+++ password_strength.py
@@ -1,23 +1,22 @@
+import getpass
+import os.path
 import sys
 
 
-black_list = [
-    '123456789',
-    'admin',
-    'qwerty',
-    'password',
-    'qweasd',
-]
+def load_black_list(filepath='black_list.txt'):
+    if os.path.exists(filepath):
+        with open(filepath) as source_file:
+            black_list = source_file.read().upper().split()
+    else:
+        black_list = []
+    return black_list
 
 
 def is_in_black_list(password):
-    if password in black_list:
-        return 0
-    else:
-        return 1
+    return password.upper() in black_list
 
 
-def length_checker(password):
+def rate_length(password):
     if len(password) < 5:
         return 0
     elif len(password) < 8:
@@ -27,20 +26,14 @@ def length_checker(password):
 
 
 def is_all_digits(password):
-    if len(password) == sum(1 for symbol in password if symbol.isdigit()):
-        return 0
-    else:
-        return 1
+    return password.isdigit()
 
 
 def is_all_letters(password):
-    if len(password) == sum(1 for symbol in password if symbol.isalpha()):
-        return 0
-    else:
-        return 1
+    return password.isalpha()
 
 
-def is_camel_case(password):
+def rate_for_camel_case(password):
     count_of_upper_case_letters = sum(
         1 for symbol in password if symbol.isupper())
     count_of_lower_case_letters = sum(
@@ -54,7 +47,7 @@ def is_camel_case(password):
         return 0
 
 
-def has_special_symbols(password):
+def rate_for_special_symbols(password):
     count_of_special_symbols = sum(
         1 for symbol in password if symbol.isalnum())
     if count_of_special_symbols in range(len(password)):
@@ -65,23 +58,31 @@ def has_special_symbols(password):
         return 0
 
 
-def get_password_strength(password):
+def result_counter(password):
+    bonus_for_all_passed = 1
     score = 1
-    if is_in_black_list(password) == 0:
+    if is_in_black_list(password) == 1:
         return 0
     else:
-        score += length_checker(password)
-        score += is_all_digits(password)
-        score += is_all_letters(password)
-        score += is_camel_case(password)
-        score += has_special_symbols(password)
-    # if all tests are passed we add 1 bonus point to final score
-    score = (score if score < 9 else 10)
+        score += sum([length_checker(password),
+                     is_all_digits(password),
+                     is_all_letters(password),
+                     is_camel_case(password),
+                     has_special_symbols(password)])
+    score = (score if score < 9 else score + bonus_for_all_passed)
+    return score
+
+
+def get_password_strength(score):
     print('On scale of 1 to 10 your password got {}'.format(score))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("program need one argument - password")
+    if len(sys.argv) > 1:
+        black_list = load_black_list(sys.argv[1])
     else:
-        get_password_strength(sys.argv[1])
+        black_list = load_black_list()
+
+    password = getpass.getpass('Please enter your password')
+    score = result_counter(password)
+    get_password_strength(score)
