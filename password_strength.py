acef615bca01c1244aec50b89123b92b4f2531df
Pavel Piskunov <pspiskunov@gmail.com>
2018-03-07 00:01:27 +0300

    упрощён код, удалены глобальные переменные, удалены магические числа, исправлено форматирование

--- password_strength.py
+++ password_strength.py
@@ -6,20 +6,22 @@ import sys
 def load_black_list(filepath='black_list.txt'):
     if os.path.exists(filepath):
         with open(filepath) as source_file:
-            black_list = source_file.read().upper().split()
+            black_list = source_file.read().upper().split('\n')
     else:
         black_list = []
     return black_list
 
 
-def is_in_black_list(password):
+def is_in_black_list(password, black_list):
     return password.upper() in black_list
 
 
 def rate_length(password):
-    if len(password) < 5:
+    too_small_length = 5
+    middle_length = 8
+    if len(password) < too_small_length:
         return 0
-    elif len(password) < 8:
+    elif len(password) < middle_length:
         return 1
     else:
         return 2
@@ -39,41 +41,35 @@ def rate_for_camel_case(password):
     count_of_lower_case_letters = sum(
         1 for symbol in password if symbol.lower())
 
-    if count_of_upper_case_letters > 0 and count_of_lower_case_letters > 0:
-        return 2
-    elif count_of_upper_case_letters > 0 or count_of_lower_case_letters > 0:
-        return 1
-    else:
-        return 0
+    return sum([bool(count_of_upper_case_letters),
+               bool(count_of_lower_case_letters),
+                ])
 
 
 def rate_for_special_symbols(password):
+    multiplier_bonus = 2
     count_of_special_symbols = sum(
         1 for symbol in password if symbol.isalnum())
-    if count_of_special_symbols in range(len(password)):
-        return 2
-    elif count_of_special_symbols > 0:
-        return 1
-    else:
-        return 0
+    return bool(count_of_special_symbols) * multiplier_bonus
 
 
-def result_counter(password):
+def result_counter(password, black_list):
     bonus_for_all_passed = 1
     score = 1
-    if is_in_black_list(password) == 1:
+    if is_in_black_list(password, black_list) == 1:
         return 0
     else:
         score += sum([rate_length(password),
                       is_all_digits(password),
                       is_all_letters(password),
                       rate_for_camel_case(password),
-                      rate_for_special_symbols(password)])
+                      rate_for_special_symbols(password),
+                      ])
     score = (score if score < 9 else score + bonus_for_all_passed)
     return score
 
 
-def get_password_strength(score):
+def result_printer(score):
     print('On scale of 1 to 10 your password got {}'.format(score))
 
 
@@ -84,5 +80,5 @@ if __name__ == '__main__':
         black_list = load_black_list()
 
     password = getpass.getpass('Please enter your password')
-    score = result_counter(password)
-    get_password_strength(score)
+    score = result_counter(password, black_list)
+    result_printer(score)
